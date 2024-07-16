8818cd6f3ef80b80bb052b6906073978c219ca5f
tmolumby <tmolumby@arcwebtech.com>
2024-07-16 08:50:47 -0500

    (task)[138]:  Added Material to admin app, updated favicon

---
+++ packages/admin/src/app/feature/admin/admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aw-app-admin',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.scss',
+})
+export default class AdminComponent {}
