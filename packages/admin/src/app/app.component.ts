fc0ca31da73434d6d185ad09968fac31ef11413c
tmolumby <tmolumby@arcwebtech.com>
2024-07-16 10:20:12 -0500

    ARCWELL-183: updated routing and components based on review feedback

--- packages/admin/src/app/app.component.ts
+++ packages/admin/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './feature/header/header.component';
+import { FooterComponent } from './feature/footer/footer.component';
 
 @Component({
   selector: 'aw-app-root',
--- packages/admin/src/app/app.routes.ts
+++ packages/admin/src/app/app.routes.ts
@@ -2,7 +2,20 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'admin',
-    loadComponent: () => import('./feature/admin/admin.component'),
+    path: '',
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./feature/dashboard/dashboard.component').then(
+            (m) => m.DashboardComponent,
+          ),
+      },
+    ],
   },
 ];
