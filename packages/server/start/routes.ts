c524c684c175c39ccea5248754a8b31b15690cdc
Tim <timgetz@gmail.com>
2024-07-16 14:33:38 -0400

    Initial AdonisJS server import/integration (#2)
    Conversion of sandbox environment work to clean base

--- packages/server/start/routes.ts
+++ packages/server/start/routes.ts
@@ -8,13 +8,9 @@
 */
 
 import router from '@adonisjs/core/services/router'
-const ProductsController = () => import('#controllers/products_controller')
 
 router.get('/', async () => {
   return {
     hello: 'world',
-    bacon: ['delicious', 'salty'],
   }
 })
-
-router.resource('products', ProductsController)
