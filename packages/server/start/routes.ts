7cac43577c8db14942a9c94903d46458ac21676f
Tim Getz <tgetz@arcwebtech.com>
2024-07-24 11:10:00 -0400

    Initiial configuration of swagger plugin

--- packages/server/start/routes.ts
+++ packages/server/start/routes.ts
@@ -8,9 +8,19 @@
 */
 
 import router from '@adonisjs/core/services/router'
+<<<<<<< Updated upstream
 import SessionController from '#controllers/session_controller'
 import RolesController from '#controllers/roles_controller'
 import UsersController from '#controllers/users_controller'
+=======
+const AuthController = () => import('#controllers/auth_controller')
+import { middleware } from '#start/kernel'
+import AutoSwagger from 'adonis-autoswagger'
+import swagger from '#config/swagger'
+const RolesController = () => import('#controllers/roles_controller')
+const UsersController = () => import('#controllers/users_controller')
+const GetAllFullUsersController = () => import('#controllers/full_user_controller')
+>>>>>>> Stashed changes
 
 router.get('/', async () => {
   return {
@@ -21,3 +31,15 @@ router.get('/', async () => {
 router.resource('auth', SessionController)
 router.resource('roles', RolesController)
 router.resource('users', UsersController)
+
+// returns swagger in YAML
+router.get('/swagger', async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger)
+})
+
+// Renders Swagger-UI and passes YAML-output of /swagger
+router.get('/docs', async () => {
+  return AutoSwagger.default.ui('/swagger', swagger)
+  // return AutoSwagger.default.scalar("/swagger"); to use Scalar instead
+  // return AutoSwagger.default.rapidoc("/swagger", "view"); to use RapiDoc instead (pass "view" default, or "read" to change the render-style)
+})
