cbc0512a17d9cadc6041c279f4ee1c89f3e886ed
Colin McCloskey <cmccloskey@arcwebtech.com>
2024-07-26 19:11:16 -0400

    Remove temporary stub boot scripts

--- packages/server/boot.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Test boot script; delete or replace once working app is included
-
-while true; do 
-    echo "🛜 Arcwell API ${ARCWELL_NODE}:${ARCWELL_KEY} $(date +"%m-%d-%Y %H:%M:%S")"
-    sleep $ARCWELL_PING
-done
