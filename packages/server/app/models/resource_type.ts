603ea9f90eb2c1f488ec4b8c854be299ed7f47c5
Mike Berlin <mberlin@arcwebtech.com>
2024-08-13 14:54:13 -0400

    model stuff

---
+++ packages/server/app/models/resource_type.ts
@@ -0,0 +1,27 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import Resource from './resource'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+
+export default class ResourceType extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare key: string
+
+  @column()
+  declare name: string
+
+  @column()
+  declare tags: string[]
+
+  @hasMany(() => Resource)
+  declare resources: HasMany<typeof Resource>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
