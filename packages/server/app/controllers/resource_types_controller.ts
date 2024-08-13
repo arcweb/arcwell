b6be916727695e45a6760f5c0910dd8839f0cb47
Mike Berlin <mberlin@arcwebtech.com>
2024-08-13 15:28:29 -0400

    controllers

--- packages/server/app/controllers/resource_types_controller.ts
+++ packages/server/app/controllers/resource_types_controller.ts
@@ -1,28 +1,52 @@
+import ResourceType from '#models/resource_type'
+import { createResourceTypeValidator } from '#validators/resource_type'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class ResourceTypesController {
   /**
    * Display a list of resource
    */
-  async index({}: HttpContext) {}
+  async index({}: HttpContext) {
+    return { data: await ResourceType.query() }
+  }
 
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  async store({ request, auth }: HttpContext) {
+    await auth.authenticate()
+    await request.validateUsing(createResourceTypeValidator)
+    const newResourceType = await ResourceType.create(request.body())
+    return { data: newResourceType }
+  }
 
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) {}
+  async show({ params }: HttpContext) {
+    return {
+      data: await ResourceType.query().where('id', params.id).firstOrFail(),
+    }
+  }
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request, auth }: HttpContext) {
+    await auth.authenticate()
+    await request.validateUsing(createResourceTypeValidator)
+    const resourceType = await ResourceType.findOrFail(params.id)
+    const updatedResourceType = await resourceType.merge(request.body()).save()
+    return { data: updatedResourceType }
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params, auth, response }: HttpContext) {
+    await auth.authenticate()
+    const resourceType = await ResourceType.findOrFail(params.id)
+    await resourceType.delete()
+    response.status(204).send('')
+  }
 }
