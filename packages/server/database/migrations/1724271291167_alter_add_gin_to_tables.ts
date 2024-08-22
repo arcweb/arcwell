7cdc6cd949d5622113de14b7d51361935b97841d
Mike Berlin <mberlin@arcwebtech.com>
2024-08-22 12:14:21 -0400

    basic test and migration

--- packages/server/database/migrations/1724271291167_alter_add_gin_to_tables.ts
+++ packages/server/database/migrations/1724271291167_alter_add_gin_to_tables.ts
@@ -4,7 +4,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.alterTable('users', (table) => {
       table.jsonb('tags').defaultTo('[]').notNullable()
-      table.index(['tags'], 'user_tags_gin', 'gin')
+      table.index(['tags'], 'users_tags_gin', 'gin')
     })
 
     this.schema.alterTable('people', (table) => {
@@ -34,32 +34,32 @@ export default class extends BaseSchema {
 
   async down() {
     this.schema.alterTable('users', (table) => {
-      table.dropIndex('user_tags_gin')
+      table.dropIndex(['tags'], 'users_tags_gin')
       table.dropColumn('tags')
     })
 
     this.schema.alterTable('people', (table) => {
-      table.dropIndex('people_tags_gin')
+      table.dropIndex(['tags'], 'people_tags_gin')
     })
 
     this.schema.alterTable('person_types', (table) => {
-      table.dropIndex('person_types_tags_gin')
+      table.dropIndex(['tags'], 'person_types_tags_gin')
     })
 
     this.schema.alterTable('resources', (table) => {
-      table.dropIndex('resources_tags_gin')
+      table.dropIndex(['tags'], 'resources_tags_gin')
     })
 
     this.schema.alterTable('resource_types', (table) => {
-      table.dropIndex('resource_types_tags_gin')
+      table.dropIndex(['tags'], 'resource_types_tags_gin')
     })
 
     this.schema.alterTable('events', (table) => {
-      table.dropIndex('events_tags_gin')
+      table.dropIndex(['tags'], 'events_tags_gin')
     })
 
     this.schema.alterTable('event_types', (table) => {
-      table.dropIndex('event_types_tags_gin')
+      table.dropIndex(['tags'], 'event_types_tags_gin')
     })
   }
 }
