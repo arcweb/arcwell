6350f8612d7b2d77601a1633e1063c74eaf6846b
Mike Berlin <mberlin@arcwebtech.com>
2024-08-22 09:39:15 -0400

    all gins

--- packages/server/database/migrations/1724271291167_alter_add_gin_to_tables.ts
+++ packages/server/database/migrations/1724271291167_alter_add_gin_to_tables.ts
@@ -4,14 +4,62 @@ export default class extends BaseSchema {
   async up() {
     this.schema.alterTable('users', (table) => {
       table.jsonb('tags').defaultTo('[]').notNullable()
-      table.index(['tags'], 'tags_gin', 'gin')
+      table.index(['tags'], 'user_tags_gin', 'gin')
+    })
+
+    this.schema.alterTable('people', (table) => {
+      table.index(['tags'], 'people_tags_gin', 'gin')
+    })
+
+    this.schema.alterTable('person_types', (table) => {
+      table.index(['tags'], 'person_types_tags_gin', 'gin')
+    })
+
+    this.schema.alterTable('resources', (table) => {
+      table.index(['tags'], 'resources_tags_gin', 'gin')
+    })
+
+    this.schema.alterTable('resource_types', (table) => {
+      table.index(['tags'], 'resource_types_tags_gin', 'gin')
+    })
+
+    this.schema.alterTable('events', (table) => {
+      table.index(['tags'], 'events_tags_gin', 'gin')
+    })
+
+    this.schema.alterTable('event_types', (table) => {
+      table.index(['tags'], 'event_types_tags_gin', 'gin')
     })
   }
 
   async down() {
     this.schema.alterTable('users', (table) => {
-      table.dropIndex('tags_gin')
+      table.dropIndex('user_tags_gin')
       table.dropColumn('tags')
+    })
+
+    this.schema.alterTable('people', (table) => {
+      table.dropIndex('people_tags_gin')
+    })
+
+    this.schema.alterTable('person_types', (table) => {
+      table.dropIndex('person_types_tags_gin')
+    })
+
+    this.schema.alterTable('resources', (table) => {
+      table.dropIndex('resources_tags_gin')
+    })
+
+    this.schema.alterTable('resource_types', (table) => {
+      table.dropIndex('resource_types_tags_gin')
+    })
+
+    this.schema.alterTable('events', (table) => {
+      table.dropIndex('events_tags_gin')
+    })
+
+    this.schema.alterTable('event_types', (table) => {
+      table.dropIndex('event_types_tags_gin')
     })
   }
 }
