33e9c7ebbaf6ee43413e077e821cfaf9c48b9310
tmolumby <80765433+tmolumby@users.noreply.github.com>
2024-09-12 11:33:26 -0400

    Feature/ar CWE ll 182 dyanmic features sub menu (#76)
    * (feature)[ARCWELL-182]: added dynamic features sub menus
    
    * feature/ARCWELL-182 minor updates based on PR review
    
    * feature/ARCWELL-182: fixed issues with feature menu logic
    
    * feature/ARCWELL-182 removed commented code and console.logs

--- packages/admin/src/app/feature/project-management/project-management.component.ts
+++ packages/admin/src/app/feature/project-management/project-management.component.ts
@@ -1,9 +1,11 @@
-import { Component, effect, inject } from '@angular/core';
+import { AfterViewInit, Component, effect, inject } from '@angular/core';
 import { AuthStore } from '@shared/store/auth.store';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { FeatureSubMenuComponent } from '@feature/project-management/feature-sub-menu/feature-sub-menu.component';
 import { FeaturesMenuComponent } from '@feature/project-management/features-menu/features-menu.component';
 import { TopMenuComponent } from '@feature/top-menu/top-menu.component';
+import { P } from '@angular/cdk/keycodes';
+import { FeatureStore } from '@app/shared/store/feature.store';
 
 @Component({
   selector: 'aw-home',
@@ -18,9 +20,10 @@ import { TopMenuComponent } from '@feature/top-menu/top-menu.component';
   templateUrl: './project-management.component.html',
   styleUrl: './project-management.component.scss',
 })
-export class ProjectManagementComponent {
+export class ProjectManagementComponent implements AfterViewInit {
   readonly authStore = inject(AuthStore);
   private router = inject(Router);
+  readonly featureStore = inject(FeatureStore);
 
   constructor() {
     effect(() => {
@@ -28,5 +31,11 @@ export class ProjectManagementComponent {
         this.router.navigate(['auth', 'login']);
       }
     });
+  }
+
+  ngAfterViewInit() {
+    if (this.authStore.currentUser()) {
+      this.featureStore.load();
+    }
   }
 }
