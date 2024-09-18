b8c12f8b8d84c4757b79c8ce7fec73a724b17a57
Mike Berlin <mberlin@arcwebtech.com>
2024-09-18 15:01:28 -0400

    inital code to setup mail hog adn andonis mail

---
+++ packages/server/config/mail.ts
@@ -0,0 +1,42 @@
+import env from '#start/env'
+import { defineConfig, transports } from '@adonisjs/mail'
+
+const mailConfig = defineConfig({
+  default: 'smtp',
+
+  from: {
+    address: '',
+    name: '',
+  },
+  replyTo: {
+    address: '',
+    name: '',
+  },
+
+  /**
+   * The mailers object can be used to configure multiple mailers
+   * each using a different transport or same transport with different
+   * options.
+   */
+  mailers: {
+    smtp: transports.smtp({
+      host: env.get('SMTP_HOST'),
+      port: env.get('SMTP_PORT'),
+      /**
+       * Uncomment the auth block if your SMTP
+       * server needs authentication
+       */
+      /* auth: {
+        type: 'login',
+        user: env.get('SMTP_USERNAME'),
+        pass: env.get('SMTP_PASSWORD'),
+      }, */
+    }),
+  },
+})
+
+export default mailConfig
+
+declare module '@adonisjs/mail/types' {
+  export interface MailersList extends InferMailers<typeof mailConfig> {}
+}
