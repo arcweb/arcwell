2477783f68b2946411a58edbe0fe73d88deb29a6
Alex Smith <asmith@arcwebtech.com>
2024-09-17 20:40:54 -0400

    health check action for build

---
+++ packages/admin/karma.conf.js
@@ -0,0 +1,27 @@
+export default function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client:{
+      clearContext: false
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_DEBUG,
+    autoWatch: true,
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', 'disable-gpu']
+      }
+    },
+    singleRun: true
+  });
+};
