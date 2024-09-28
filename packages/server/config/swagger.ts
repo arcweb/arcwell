0770ca836420bc97526c5b58a1ad17f99c357461
Colin McCloskey <cmccloskey@arcwebtech.com>
2024-09-28 16:00:54 -0400

    Install adonis-autoswagger, initial configuration

---
+++ packages/server/config/swagger.ts
@@ -0,0 +1,35 @@
+import path from 'node:path'
+import url from 'node:url'
+
+export default {
+  // path: __dirname + "/../", for AdonisJS v5
+  path: path.dirname(url.fileURLToPath(import.meta.url)) + '/../', // for AdonisJS v6
+  tagIndex: 1,
+  info: {
+    title: 'Arcwell Server',
+    version: '0.1.0',
+    description: 'Arcwell Digital Medicine Platform',
+    license: {
+      name: 'Apache-2.0',
+      url: 'http://www.apache.org/licenses/LICENSE-2.0.html',
+    },
+    externalDocs: {
+      description: 'Learn more about the Arcwell Server and its implementation',
+      url: 'https://github.com/arcweb/arcwell',
+    },
+  },
+  snakeCase: false,
+
+  debug: true, // set to true, to get some useful debug output
+  ignore: ['/swagger.yml', '/docs'],
+  preferredPutPatch: 'PATCH',
+  common: {
+    parameters: {}, // OpenAPI conform parameters that are commonly used
+    headers: {}, // OpenAPI conform headers that are commonly used
+  },
+  securitySchemes: {}, // optional
+  authMiddlewares: ['auth', 'auth:api'], // optional
+  defaultSecurityScheme: 'BearerAuth', // optional
+  persistAuthorization: true, // persist authorization between reloads on the swagger page
+  showFullPath: true, // the path displayed after endpoint summary
+}
--- packages/server/start/routes.ts
+++ packages/server/start/routes.ts
@@ -30,6 +30,18 @@ const TagsController = () => import('#controllers/tags_controller')
 const HealthChecksController = () => import('#controllers/health_checks_controller')
 router.get('/health', [HealthChecksController])
 
+import AutoSwagger from 'adonis-autoswagger'
+import swagger from '#config/swagger'
+
+router.get('/swagger.yml', async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger)
+})
+router.get('/docs', async () => {
+  //return AutoSwagger.default.ui('/swagger.yml', swagger)
+  return AutoSwagger.default.scalar('/swagger.yml') // to use Scalar instead
+  //return AutoSwagger.default.rapidoc('/swagger.yml', 'read') //to use RapiDoc instead (pass "view" default, or "read" to change the render-style)
+})
+
 router.get('/', async () => {
   return {
     arcwell: {
