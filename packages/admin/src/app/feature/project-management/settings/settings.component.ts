8a1834ba7ec1677dc80ad2d5f66067a263ad93aa
Mike Berlin <mberlin@arcwebtech.com>
2024-10-02 16:13:10 -0400

    viewable instance info

--- packages/admin/src/app/feature/project-management/settings/settings.component.ts
+++ packages/admin/src/app/feature/project-management/settings/settings.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { KeyValuePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
+import { ConfigStore } from '@app/shared/store/config.store';
 
 @Component({
   selector: 'aw-settings',
   standalone: true,
-  imports: [],
+  imports: [MatCard, MatCardHeader, MatCardContent, KeyValuePipe],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrl: './settings.component.scss',
 })
 export class SettingsComponent {
-
+  readonly configStore = inject(ConfigStore);
 }
