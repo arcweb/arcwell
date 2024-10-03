7e2e559d7e9be2ea101f14a634f276a441b6fc50
Mike Berlin <mberlin@arcwebtech.com>
2024-10-03 12:27:45 -0400

    initial code

--- packages/admin/src/app/feature/project-management/dashboard/dashboard.component.ts
+++ packages/admin/src/app/feature/project-management/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCard, MatCardContent, MatCardFooter } from '@angular/material/card';
+import { MatLabel } from '@angular/material/form-field';
 
 @Component({
   selector: 'aw-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatCard, MatLabel, MatCardContent, MatCardFooter],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
