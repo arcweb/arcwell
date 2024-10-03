8fd29c8ced49f0dbdc0f95ee5a1d9d99b1181c91
Mike Berlin <mberlin@arcwebtech.com>
2024-10-03 10:26:16 -0400

    fully styled

--- packages/admin/src/app/feature/project-management/settings/settings.component.ts
+++ packages/admin/src/app/feature/project-management/settings/settings.component.ts
@@ -1,15 +1,40 @@
 import { KeyValuePipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 import { ConfigStore } from '@app/shared/store/config.store';
 
 @Component({
   selector: 'aw-settings',
   standalone: true,
-  imports: [MatCard, MatCardHeader, MatCardContent, KeyValuePipe],
+  imports: [
+    KeyValuePipe,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatDivider,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule,
+  ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
 })
 export class SettingsComponent {
   readonly configStore = inject(ConfigStore);
+
+  instanceForm = new FormGroup({
+    instanceName: new FormControl({
+      value: this.configStore.config()?.arcwell.name,
+      disabled: false,
+    }),
+    instanceId: new FormControl({
+      value: this.configStore.config()?.arcwell?.id,
+      disabled: false,
+    }),
+  });
 }
