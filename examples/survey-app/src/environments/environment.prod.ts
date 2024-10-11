5bbd10ae44bb78274b0f71d61763f94709403335
Colin McCloskey <cmccloskey@arcwebtech.com>
2024-10-11 13:34:18 -0400

    Update Postman and Example Survey to reflect new /api/v1 server paths

--- examples/survey-app/src/environments/environment.prod.ts
+++ examples/survey-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['PRODUCTION_API_URL'] || 'http://localhost:3333',
+  apiUrl: process.env['PRODUCTION_API_URL'] || 'http://localhost:3333/api/v1',
 };
--- examples/survey-app/src/environments/environment.ts
+++ examples/survey-app/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: process.env['DEVELOPMENT_API_URL'] || 'http://localhost:3333',
+  apiUrl: process.env['DEVELOPMENT_API_URL'] || 'http://localhost:3333/api/v1',
 };
 
 /*
