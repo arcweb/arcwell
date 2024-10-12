a7f37c7c4f0b235dad3b10ddca4a5b8dd6b8b1b9
Tim Getz <tgetz@arcwebtech.com>
2024-10-12 01:32:32 -0400

    feature/ARCWELL-373-all-object-dimension-schema - Replace info column with dimensions - Added dimension_schemas on all types - Fixed typescript issues with cohorts - Fixed typescript errors with all controllers - Fixed typescript errors in handler and routes - All linting errors removed in server - Refactored cohort zod schemas - Added eslint 9 support (beta) - Updated eslint config which adds linting for edge templates - Added prettier config (copied Adonisjs defaults) - Eslint will now launch prettier with prettierrc config

--- packages/server/eslint.config.js
+++ packages/server/eslint.config.js
@@ -1,12 +1,14 @@
 import { configApp } from '@adonisjs/eslint-config'
+// const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
+import prettier from 'eslint-plugin-prettier'
+
 export default configApp({
   name: 'Custom config',
   // files: INCLUDE_LIST,
   ignores: ['**/index.html'],
-  plugins: {
-    // ESLint plugins go here
-  },
+  plugins: { prettier: prettier },
   rules: {
     // ESLint rules go here
+    'prettier/prettier': 'error',
   },
 })
