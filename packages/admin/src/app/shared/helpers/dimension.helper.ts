9f6a4962e45fa17b30d5a7f5ddcd3cdc09ecf238
Tim Getz <tgetz@arcwebtech.com>
2024-10-20 03:38:56 -0400

    feature/admin-dimensions-schema-ui - Added Create/Update Dimension Schemas table/dialog for Fact Types, Event Types, Person Types, Resource Types - Added Create/Update Dimension table/dialog for Fact, Event, Person, Resource - Moved Dimension & Dimension Schema logic to a reusable components - Fixed paging through events (was trying to sort on Name which doesn't exisit on events)

--- packages/admin/src/app/shared/helpers/dimension.helper.ts
+++ packages/admin/src/app/shared/helpers/dimension.helper.ts
@@ -43,47 +43,13 @@ export function convertDimensionDataTypeValues<
       if (!isNaN(num)) {
         dimension.value = num;
       } else {
-        console.warn(
+        throw new Error(
           `Invalid number for dimension ${dimension.key}: ${dimension.value}`,
         );
       }
     }
-    // Add more data types as needed (e.g., 'boolean', 'date', etc.)
+    // TODO: Add boolean dataType functionality
   });
 
   return obj;
 }
-
-// import { FactType } from '@schemas/fact.schema';
-// import { DimensionSchemaType } from '@schemas/dimension-schema.schema';
-// import { DimensionType } from '@schemas/dimension.schema';
-//
-// export function convertDimensionDataTypeValues(obj: FactType): FactType {
-//   const { dimensionSchemas } = obj.factType;
-//   const { dimensions } = obj;
-//
-//   // Create a map of dimension keys to their data types
-//   const dataTypeMap = new Map<string, string>(
-//     dimensionSchemas.map((schema: DimensionSchemaType) => [
-//       schema.key,
-//       schema.dataType,
-//     ]),
-//   );
-//
-//   // Convert dimension values based on their data type
-//   dimensions.forEach((dimension: DimensionType) => {
-//     const dataType = dataTypeMap.get(dimension.key);
-//     if (dataType === 'number') {
-//       const num = Number(dimension.value);
-//       if (!isNaN(num)) {
-//         dimension.value = num;
-//       } else {
-//         console.warn(
-//           `Invalid number for dimension ${dimension.key}: ${dimension.value}`,
-//         );
-//       }
-//     }
-//   });
-//
-//   return obj;
-// }
