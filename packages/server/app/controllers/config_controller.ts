40e1ceb330ba8cb8698bf2170afc17a36114eabd
Mike Berlin <mberlin@arcwebtech.com>
2024-10-30 14:21:22 -0400

    organization of the config data

--- packages/server/app/controllers/config_controller.ts
+++ packages/server/app/controllers/config_controller.ts
@@ -37,6 +37,7 @@ export default class ConfigController {
         name: env.get('ARCWELL_INSTANCE_NAME'),
         id: env.get('ARCWELL_INSTANCE_ID'),
         cors: env.get('ARCWELL_SERVER_CORS_ORIGINS'),
+        debugError: env.get('ARCWELL_SERVER_DEBUG_ERRORS')
       },
       mail: {
         host: env.get('SMTP_HOST'),
