01ced04d5db9c225a95d03a932456d3fc3a8974d
Tim Getz <tgetz@arcwebtech.com>
2024-10-30 12:11:40 -0400

    testing ci

--- packages/server/app/validators/dimension.ts
+++ packages/server/app/validators/dimension.ts
@@ -42,7 +42,10 @@ const vineBooleanSchema = vine.compile(vine.boolean())
  * @returns {Promise<string[]>} - Returns an array of error messages if validation fails, or an empty array if the value is valid.
  * @throws {Error} - Throws an error if the data type is unrecognized.
  */
-async function validateDimensionSchema(value: string, dataType: string): Promise<string[]> {
+async function validateDimensionSchema(
+  value: string | number,
+  dataType: string
+): Promise<string[]> {
   const errors: string[] = []
 
   switch (dataType.toLowerCase()) {
