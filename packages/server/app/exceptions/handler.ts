2b9467f80a7045f81c6a2e57bb6096adf56920da
Mike Berlin <mberlin@arcwebtech.com>
2024-10-31 10:23:53 -0400

    modified helper to use new env var

--- packages/server/app/exceptions/handler.ts
+++ packages/server/app/exceptions/handler.ts
@@ -4,6 +4,8 @@ import { errors as vineErrors } from '@vinejs/vine'
 import { errors as adonisCoreErrors } from '@adonisjs/core'
 import { errors as authErrors } from '@adonisjs/auth'
 import { errors as lucidErrors } from '@adonisjs/lucid'
+import env from '#start/env'
+import { Console } from 'console'
 // Keeping this commented out import as a reminder when we implement polices
 // import { errors as bouncerErrors } from '@adonisjs/bouncer'
 
@@ -152,7 +154,14 @@ export default class HttpExceptionHandler extends ExceptionHandler {
       const err = error as UnknownDatabaseError
       let title = 'Unknown Database Error'
       let code
-      const detail = err && err.message ? err.message : 'No Further Information'
+
+      let detail
+      if (env.get('ARCWELL_SERVER_DEBUG_ERRORS') === 'true') {
+        console.log('\n\nGENERATING DETAIL\n\n')
+        detail = err && err.message ? err.message : 'No Further Information'
+      } else {
+        detail = 'A server error occurred processing that request'
+      }
 
       switch (err.code) {
         case '23503': {
