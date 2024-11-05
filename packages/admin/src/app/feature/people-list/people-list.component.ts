e477f69f4552a2214099a55ec7f2195dde10df1e
Tim <timgetz@gmail.com>
2024-11-05 16:07:11 -0500

    Feature/ci cypress (#221)
    * feature/ARCWELL-387-admin-testing
    
    * feature/ARCWELL-387-admin-unit-testing-poc
    
    * Added component testing with Chromium in github action
    
    * Added component testing with Chromium in github action
    
    * Added component testing with Chromium in github action
    
    * Added component testing with Chromium in github action
    
    * Added component testing with Chromium in github action
    
    * Added component testing with Chromium in github action
    
    * Added component testing with Chromium in github action
    
    * Added component testing with Chromium in github action
    
    ---------
    
    Co-authored-by: tmolumby <tmolumby@arcwebtech.com>

--- packages/admin/src/app/feature/people-list/people-list.component.ts
+++ packages/admin/src/app/feature/people-list/people-list.component.ts
@@ -62,6 +62,7 @@ export class PeopleListComponent {
   constructor() {
     effect(() => {
       this.dataSource.data = this.peopleListStore.people();
+      console.log(this.peopleListStore.totalData());
     });
     // load the people list based on the route parameters if they exist
     this.typeKey$.subscribe(typeKey => {
