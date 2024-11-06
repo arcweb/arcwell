3d06f86b2c1830d85eaa1b55facf4f23b35edbdd
Mike Berlin <mberlin@arcwebtech.com>
2024-11-06 15:08:44 -0500

    linter fixes

--- packages/server/app/controllers/people_controller.ts
+++ packages/server/app/controllers/people_controller.ts
@@ -1,5 +1,5 @@
 import Person from '#models/person'
-import fs from 'node:fs';
+import fs from 'node:fs'
 import PersonType from '#models/person_type'
 import { paramsUUIDValidator } from '#validators/common'
 import {
@@ -14,8 +14,8 @@ import { ExtractScopes } from '@adonisjs/lucid/types/model'
 import PersonService from '#services/person_service'
 import { validateDimensions } from '#validators/dimension'
 import { throwCustomHttpError } from '#exceptions/handler_helper'
-import { parseBulkCsv } from '#helpers/bulk_parsing';
-import { bulkUploadValidator } from '#validators/bulk';
+import { parseBulkCsv } from '#helpers/bulk_parsing'
+import { bulkUploadValidator } from '#validators/bulk'
 
 export default class PeopleController {
   /**
@@ -236,10 +236,10 @@ export default class PeopleController {
   async bulk({ request, response }: HttpContext) {
     await request.validateUsing(bulkUploadValidator)
     const file = request.file('file')
-    const trx = await db.transaction() 
+    const trx = await db.transaction()
     if (file) {
-      const csvFile = fs.readFileSync(file.tmpPath!, 'utf8');
-      parseBulkCsv(trx,  csvFile, PersonService);
+      const csvFile = fs.readFileSync(file.tmpPath!, 'utf8')
+      parseBulkCsv(trx, csvFile, PersonService)
     } else {
       response.status(404).send('File not found')
     }
