a9c603cc1f39450bc6842e6c21ff7af59aeee87b
Mike Berlin <mberlin@arcwebtech.com>
2024-11-07 14:12:47 -0500

    bug handleingx

--- packages/admin/src/app/shared/schemas/event.schema.ts
+++ packages/admin/src/app/shared/schemas/event.schema.ts
@@ -1,9 +1,10 @@
 import { z } from 'zod';
-import { EventTypeSchema } from './event-type.schema';
+
 import { EventModel } from '../models/event.model';
 import { PersonSchema } from '@schemas/person.schema';
 import { ResourceSchema } from '@schemas/resource.schema';
 import { DimensionSchema, serializeDimension } from '@schemas/dimension.schema';
+import { EventTypeSchema } from './event-type.schema';
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 export const EventSchema: any = z
